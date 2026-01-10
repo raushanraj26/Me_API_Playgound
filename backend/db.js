a2e070a0c8b54f2268129308cd82ae75f6ce304b
Raushan Raj <raushanraj3107@gmail.com>
2026-01-10 21:12:17 +0530

    shift to mongodb atlas and create vercel.json file

--- backend/db.js
+++ backend/db.js
@@ -1,8 +1,11 @@
 const mongoose = require("mongoose");
+const dotenv = require("dotenv");
+dotenv.config();
 
 const connectDB = async () => {
   try {
-    await mongoose.connect("mongodb://127.0.0.1:27017/profileDB"); //profileDB is databse name
+   // await mongoose.connect("mongodb://127.0.0.1:27017/profileDB"); //profileDB is databse name
+   await mongoose.connect(process.env.MONGODB_URI);  //using env variable for connecting to mongodb
     console.log("MongoDB connected");
   } catch (error) {
     console.error("MongoDB connection failed", error);
